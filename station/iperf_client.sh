9287b5aac3c273cc60132a2d7ffd08add844b597
= <=>
2017-10-21 20:25:32 -0200

    Setting bandwidth to 27Kbits/s on iperf and expecting 20 pakages on tcpdump

--- station/iperf_client.sh
+++ station/iperf_client.sh
@@ -17,13 +17,14 @@ echo "Executing test number $COUNTER"
 echo "Su password needed"
 sudo echo "Thanks!"
 
-sudo tcpdump -i $2 udp port 5100 -vvv -ttt -c 800 -w wlan_$5.pcap &
-sudo tcpdump -i $3 udp port 5100 -vvv -ttt -c 800 -w eth_$5.pcap &
+sudo tcpdump -i $2 udp port 5100 -vvv -ttt -c 20 -w wlan_$5.pcap &
+sudo tcpdump -i $3 udp port 5100 -vvv -ttt -c 20 -w eth_$5.pcap &
 
 sleep 1
-iperf -c $4 -u -i 1 -f k -p 5100 -S 0x06 > iperf_$5.log 
+iperf -c $4 -b 27k -u -i 1 -f k -p 5100 -S 0x06 > iperf_$5.log 
 
-
+sleep 5
+sudo killall tcpdump
 echo "Test $5 finished."
 
 exit 0
