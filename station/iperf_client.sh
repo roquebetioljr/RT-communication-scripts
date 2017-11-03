75c0c92e622a96ed548c1b13ddb38147776ddc87
Roque Betiol Jr <roque.betioljr@gmail.com>
2017-11-03 17:45:02 -0200

    Using iperf (v2)

--- station/iperf_client.sh
+++ station/iperf_client.sh
@@ -20,14 +20,14 @@ sudo echo "Thanks!"
 sudo tcpdump -i $2 udp port 5100 -vvv -ttt -c 19500 -w wlan_$5.pcap &
 sudo tcpdump -i $3 udp port 5100 -vvv -ttt -c 19500 -w eth_$5.pcap &
 
-iperf3 -s -i 1 -f k -p 5100 >> iperf_server_$5.log &
+iperf -s -u -i 1 -f k -p 5100 >> iperf_server_$5.log &
 
 sleep 1
 # for RT stations
-iperf3 -c $4 -b 36k -u -i 1 -f l -p 5100 -S 0x06 -k 1000 -l 45 > iperf_$5.log
+iperf -c $4 -b 36k -u -i 1 -f l -p 5100 -S 0x06 -k 1000 -l 45 > iperf_$5.log
 
 # for NRT stations
-#iperf3 -c $4 -b 9M -u -i 1 -f l -p 5100 > iperf_$5.log
+#iperf -c $4 -b 9M -u -i 1 -f l -p 5100 > iperf_$5.log
 
 sleep 5
 sudo killall tcpdump
